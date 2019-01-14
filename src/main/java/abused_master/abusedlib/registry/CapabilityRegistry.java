67f08f0dc82535957f1b2d9b4f2b0a120b505783
abused_master <techperson71@gmail.com>
2019-01-13 21:56:33 -0500

    Improved capability registry

--- src/main/java/abused_master/abusedlib/registry/CapabilityRegistry.java
+++ src/main/java/abused_master/abusedlib/registry/CapabilityRegistry.java
@@ -3,14 +3,16 @@ package abused_master.abusedlib.registry;
 import abused_master.abusedlib.AbusedLib;
 import abused_master.abusedlib.capabilities.Capability;
 import abused_master.abusedlib.capabilities.CapabilityHandler;
+import abused_master.abusedlib.capabilities.defaults.inventory.CapabilityItemHandler;
 import abused_master.abusedlib.capabilities.utils.CapabilityInject;
 import abused_master.abusedlib.capabilities.utils.RegisterCapability;
-import abused_master.abusedlib.capabilities.defaults.inventory.CapabilityItemHandler;
 import net.minecraft.util.Identifier;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,13 +21,22 @@ import java.util.function.Function;
 
 public class CapabilityRegistry {
 
-    private static Map<Identifier, Class> capabilitiesMap = new HashMap<>();
-    private static List<Function<Capability<?>, Object>> functions = new ArrayList();
+    public CapabilityRegistry INSTANCE = new CapabilityRegistry();
+
+    private final Map<Identifier, Class> capabilitiesMap;
+    private final List<Function<Capability<?>, Object>> functions;
+    private final MethodHandles.Lookup methodLookup;
+
+    public CapabilityRegistry() {
+        capabilitiesMap = new HashMap<>();
+        functions = new ArrayList<>();
+        methodLookup = MethodHandles.lookup();
+    }
 
     /**
      * Add default capabilities to list
      */
-    public static void addCapabilities() {
+    public void addCapabilities() {
         capabilitiesMap.put(new Identifier(AbusedLib.MODID, "capability_item_handler"), CapabilityItemHandler.class);
     }
 
@@ -43,44 +54,60 @@ public class CapabilityRegistry {
 
     /**
      * Register a custom capability
+     *
      * @param identifier - The unique identifier for the capability
-     * @param clazz - The Capability.class to register
+     * @param clazz      - The Capability.class to register
      */
-    public static void registerCapability(Identifier identifier, Class clazz) {
+    public void registerCapability(Identifier identifier, Class clazz) {
         capabilitiesMap.put(identifier, clazz);
         registerCapability(clazz);
     }
 
     /**
      * Called to get and invoke/set the RegisterCapability and CapabilityInject annotations
+     *
      * @param clazz - The capability class
      */
-    private static void registerCapability(Class clazz) {
+    private void registerCapability(Class clazz) {
         for (Method method : clazz.getDeclaredMethods()) {
-            if(method.isAnnotationPresent(RegisterCapability.class)) {
+            if (!method.isAnnotationPresent(RegisterCapability.class)) {
+                continue;
+            }
+
+            if (Modifier.isPublic(method.getModifiers())) {
+                throw new UnsupportedOperationException("RegisterCapability methods must be public. Method: " + method.getName());
+            }
+
+            if (method.getParameterTypes().length > 0) {
+                throw new UnsupportedOperationException("RegisterCapability methods must not contain any parameters. Method " + method.getName());
+            }
+
+            try {
+                MethodHandle methodHandle = methodLookup.unreflect(method).bindTo(clazz);
+                methodHandle.invoke(null);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        }
+
+        for (Field field : clazz.getDeclaredFields()) {
+            if (!field.isAnnotationPresent(CapabilityInject.class)) {
+                continue;
+            }
+
+            functions.add(capability -> {
                 try {
-                    method.invoke(null, null);
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (InvocationTargetException e) {
+                    CapabilityHandler.INSTANCE.setFailsafeFieldValue(field, null, capability);
+                } catch (Exception e) {
+                    AbusedLib.LOGGER.warning("Unable to inject capability Name: " + field.getName() + " Target Class: " + clazz.getName());
                     e.printStackTrace();
                 }
-            }
 
-            for (Field field : clazz.getDeclaredFields()) {
-                if(field.isAnnotationPresent(CapabilityInject.class)) {
-                    functions.add(capability -> {
-                        try {
-                            CapabilityHandler.INSTANCE.setFailsafeFieldValue(field, null, capability);
-                        } catch (Exception e) {
-                            AbusedLib.LOGGER.warning("Unable to inject capability Name: " + field.getName() + " Target Class: " + clazz.getName());
-                            e.printStackTrace();
-                        }
+                return null;
+            });
 
-                        return null;
-                    });
-                }
-            }
         }
     }
 }
