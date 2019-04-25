2aecca615bd59e08d6b9beced18f7278ac14eb9b
abused_master <techperson71@gmail.com>
2019-04-25 19:26:54 -0400

    Added transfer slot helper methods

--- src/main/java/abused_master/abusedlib/utils/InventoryHelper.java
+++ src/main/java/abused_master/abusedlib/utils/InventoryHelper.java
@@ -1,12 +1,18 @@
 package abused_master.abusedlib.utils;
 
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.container.Container;
+import net.minecraft.container.Slot;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.SidedInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class InventoryHelper {
 
@@ -67,4 +73,97 @@ public class InventoryHelper {
 
         return null;
     }
+
+    //Credits to Diesieben07 for this method
+    public static ItemStack handleShiftClick(Container container, PlayerEntity player, int slotIndex) {
+        @SuppressWarnings("unchecked")
+        List<Slot> slots = container.slotList;
+        Slot sourceSlot = slots.get(slotIndex);
+        ItemStack inputStack = sourceSlot.getStack();
+        if (inputStack == ItemStack.EMPTY) {
+            return ItemStack.EMPTY;
+        }
+
+        boolean sourceIsPlayer = sourceSlot.inventory == player.inventory;
+
+        ItemStack copy = inputStack.copy();
+
+        if (sourceIsPlayer) {
+            if (!mergeStack(player.inventory, false, sourceSlot, slots, false)) {
+                return ItemStack.EMPTY;
+            } else {
+                return copy;
+            }
+        } else {
+            boolean isMachineOutput = !sourceSlot.canTakeItems(player);
+            if (!mergeStack(player.inventory, true, sourceSlot, slots, !isMachineOutput)) {
+                return ItemStack.EMPTY;
+            } else {
+                return copy;
+            }
+        }
+    }
+
+    private static boolean mergeStack(PlayerInventory playerInv, boolean mergeIntoPlayer, Slot sourceSlot, List<Slot> slots, boolean reverse) {
+        ItemStack sourceStack = sourceSlot.getStack();
+
+        int originalSize = sourceStack.getAmount();
+
+        int len = slots.size();
+        int idx;
+
+        if (sourceStack.canStack()) {
+            idx = reverse ? len - 1 : 0;
+
+            while (sourceStack.getAmount() > 0 && (reverse ? idx >= 0 : idx < len)) {
+                Slot targetSlot = slots.get(idx);
+                if ((targetSlot.inventory == playerInv) == mergeIntoPlayer) {
+                    ItemStack target = targetSlot.getStack();
+                    if (ItemStack.areEqual(sourceStack, target)) {
+                        int targetMax = Math.min(targetSlot.getMaxStackAmount(), target.getMaxAmount());
+                        int toTransfer = Math.min(sourceStack.getAmount(), targetMax - target.getAmount());
+                        if (toTransfer > 0) {
+                            target.addAmount(toTransfer);
+                            sourceSlot.takeStack(toTransfer);
+                            targetSlot.markDirty();
+                        }
+                    }
+                }
+
+                if (reverse) {
+                    idx--;
+                } else {
+                    idx++;
+                }
+            }
+            if (sourceStack.getAmount() == 0) {
+                sourceSlot.setStack(ItemStack.EMPTY);
+                return true;
+            }
+        }
+
+        idx = reverse ? len - 1 : 0;
+        while (reverse ? idx >= 0 : idx < len) {
+            Slot targetSlot = slots.get(idx);
+            if ((targetSlot.inventory == playerInv) == mergeIntoPlayer
+                    && !targetSlot.hasStack() && targetSlot.canInsert(sourceStack)) {
+                targetSlot.setStack(sourceStack);
+                sourceSlot.setStack(ItemStack.EMPTY);
+                return true;
+            }
+
+            if (reverse) {
+                idx--;
+            } else {
+                idx++;
+            }
+        }
+
+        if (sourceStack.getAmount() != originalSize) {
+            sourceSlot.markDirty();
+            return true;
+        }
+        return false;
+    }
+
 }
