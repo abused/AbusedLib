b265e1816255436afcf5af535963e064823172e4
abused_master <techperson71@gmail.com>
2019-04-29 15:50:52 -0400

    Added a much better config system

--- src/main/java/abused_master/abusedlib/utils/Config.java
+++ src/main/java/abused_master/abusedlib/utils/Config.java
@@ -1,117 +1,197 @@
 package abused_master.abusedlib.utils;
 
 import abused_master.abusedlib.AbusedLib;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 import net.fabricmc.loader.api.FabricLoader;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
+/**
+ * A Config helper class for creating and reading from json configs
+ */
 public class Config {
 
-    private JsonObject config;
+    private String name;
     private File configFile;
-    private InputStream defaultConfigFile;
-    private File configFolderFile = FabricLoader.getInstance().getConfigDirectory();
+    private JsonObject config;
+    private Gson gson;
 
-    public Config(String modid, Class mainModClass) {
-        this(modid, mainModClass, "", false);
-    }
-
-    public Config(String modid, Class mainModClass, String name) {
-        this(modid, mainModClass, name, false);
-    }
-
-    public Config(String modid, Class mainModClass, boolean createModConfigFolder) {
-        this(modid, mainModClass, "", createModConfigFolder);
-    }
-
-    public Config(String modid, Class mainModClass, String name, boolean createModConfigFolder) {
-        this.defaultConfigFile = mainModClass.getClassLoader().getResourceAsStream("assets/" + modid + "/config.json");
-        this.runConfigSetup(modid, createModConfigFolder, name.equals("") ? false : true, name);
-    }
-
-    private void runConfigSetup(String modid, boolean createModConfigFolder, boolean hasCustomName, String customConfigName) {
-        if(!configFolderFile.exists()) {
-            configFolderFile.mkdir();
-        }
-
-        if(!createModConfigFolder) {
-            this.configFile = new File(configFolderFile.getPath() + "/" + (hasCustomName ? customConfigName : modid) + ".json");
-        }else {
-            this.configFile = new File(configFolderFile.getPath() + "/" + modid + "/" + (hasCustomName ? customConfigName : modid) + ".json");
-        }
-
-        if (defaultConfigFile == null) {
-            AbusedLib.LOGGER.fatal("Unable to find the default config.json for mod ", modid, " in assets/", modid);
-            System.exit(-1);
+    /**
+     * Constructor for creating a config using properties in a map
+     * @param name - The name of the config file
+     * @param properties - The properties to append to the config
+     */
+    public Config(String name, Map<String, Object> properties) {
+        this(name);
+        if(configFile.exists()) {
+            this.loadConfig();
             return;
-        }
-
-        if(!configFile.exists()) {
-            AbusedLib.LOGGER.info("Creating config file for {}", modid);
-
-            try (PrintWriter printWriter = new PrintWriter(new FileWriter(configFile))){
-                configFile.createNewFile();
-
-                List<String> linesList = new ArrayList<>();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(defaultConfigFile));
-
-                while (reader.ready()) {
-                    linesList.add(reader.readLine());
+        }else {
+            try(FileWriter writer = new FileWriter(configFile)) {
+                for (Map.Entry<String, Object> property : properties.entrySet()) {
+                    if (property.getValue() instanceof JsonElement) {
+                        config.add(property.getKey(), (JsonElement) property.getValue());
+                    }else if (property.getValue() instanceof Integer) {
+                        config.addProperty(property.getKey(), (int) property.getValue());
+                    }else if (property.getValue() instanceof Boolean) {
+                        config.addProperty(property.getKey(), (boolean) property.getValue());
+                    }else if (property.getValue() instanceof String) {
+                        config.addProperty(property.getKey(), (String) property.getValue());
+                    }else {
+                        AbusedLib.LOGGER.error("Unable to parse json config property! File: " + name + ".json with Property: " + property.getKey());
+                        continue;
+                    }
                 }
 
-                for (String string : linesList) {
-                    printWriter.println(string);
-                }
+                this.saveConfig(writer);
             } catch (IOException e) {
                 AbusedLib.LOGGER.fatal("You done borked something with your config", e);
             }
-        }else {
-            AbusedLib.LOGGER.info("Loading config file for {}", modid);
-            this.loadConfigFile();
         }
     }
 
-    public void loadConfigFile() {
+    /**
+     * Constructor for defining a new config using a name
+     * Append properties using the add methods from this class
+     * Save config once properties are added using saveConfig
+     * @param name - The name of the config file
+     */
+    public Config(String name) {
+        this.name = name;
+        this.configFile = new File(FabricLoader.getInstance().getConfigDirectory() + "/" + name + ".json");
+        this.config = new JsonObject();
+        this.gson = new GsonBuilder().setPrettyPrinting().create();
+    }
+
+    /**
+     * Add an integer to the config file
+     * @param key - The name of the key
+     * @param value - The {@link Integer} value
+     */
+    public void addInt(String key, int value) {
+        this.config.addProperty(key, value);
+    }
+
+    /**
+     * Add a boolean to the config file
+     * @param key - The name of the key
+     * @param value - The {@link Boolean} value
+     */
+    public void addBoolean(String key, boolean value) {
+        this.config.addProperty(key, value);
+    }
+
+    /**
+     * Add a string to the config file
+     * @param key - The name of the key
+     * @param value - The {@link String} value
+     */
+    public void addString(String key, String value) {
+        this.config.addProperty(key, value);
+    }
+
+    /**
+     * Add a JsonElement to the config file
+     * @param key - The name of the key
+     * @param value - The {@link JsonElement} value
+     */
+    public void addJsonElement(String key, JsonElement value) {
+        this.config.add(key, value);
+    }
+
+    /**
+     * Get an {@link Integer} from the config
+     * @param key - The name of the entry
+     * @return - The return value
+     */
+    public int getInt(String key) {
+        return this.config.has(key) ? this.config.get(key).getAsInt() : 0;
+    }
+
+    /**
+     * Get a {@link Boolean} from the config
+     * @param key - The name of the entry
+     * @return - The return value
+     */
+    public boolean getBoolean(String key) {
+        return this.config.has(key) ? this.config.get(key).getAsBoolean() : false;
+    }
+
+    /**
+     * Get a {@link String} from the config
+     * @param key - The name of the entry
+     * @return - The return value
+     */
+    public String getString(String key) {
+        return this.config.has(key) ? this.config.get(key).getAsString() : "";
+    }
+
+    /**
+     * Get a {@link JsonElement} from the config
+     * @param key - The name of the entry
+     * @return - The return value
+     */
+    public JsonElement getJsonElement(String key) {
+        return this.config.has(key) ? this.config.get(key) : null;
+    }
+
+    /**
+     * Get the {@link JsonObject} config
+     * @return - The set {@link JsonObject}
+     */
+    public JsonObject getConfig() {
+        return config;
+    }
+
+    /**
+     * Get the {@link File} for the config
+     * @return - The config {@link File}
+     */
+    public File getConfigFile() {
+        return configFile;
+    }
+
+    /**
+     * Load an existing config file
+     */
+    private void loadConfig() {
+        if(!configFile.exists()) {
+            AbusedLib.LOGGER.error("Unable to load config file " + name + ".json, File not found!");
+            return;
+        }
+
         try {
-            config = new JsonObject();
+            AbusedLib.LOGGER.info("Loading config file " + name + ".json");
             JsonParser parser = new JsonParser();
             config = parser.parse(new FileReader(configFile)).getAsJsonObject();
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            AbusedLib.LOGGER.fatal("You done borked something with your config", e);
         }
     }
 
-    public String getString(String name) {
-        return getJsonConfig().get(name).getAsString();
+    /**
+     * Save a config file
+     * Called in the constructor after properties are added
+     * @param writer - The writer to to write with
+     */
+    private void saveConfig(FileWriter writer) {
+        AbusedLib.LOGGER.info("Creating config file " + name + ".json");
+        gson.toJson(config, writer);
     }
 
-    public int getInt(String name) {
-        return getJsonConfig().get(name).getAsInt();
-    }
-
-    public double getDouble(String name) {
-        return getJsonConfig().get(name).getAsDouble();
-    }
-
-    public long getLong(String name) {
-        return getJsonConfig().get(name).getAsLong();
-    }
-
-    public boolean getBoolean(String name) {
-        //System.out.println(config.toString());
-        return true;
-    }
-
-    public JsonArray getList(String name) {
-        return getJsonConfig().get(name).getAsJsonArray();
-    }
-
-    public JsonObject getJsonConfig() {
-        return config;
+    /**
+     * Save a config file if it doesnt exist, otherwise attempt to load it
+     * Called after adding all properties to the config manually
+     */
+    public void saveConfig() {
+        if(!configFile.exists()) {
+            try {
+                AbusedLib.LOGGER.info("Creating config file " + name + ".json");
+                gson.toJson(config, new FileWriter(configFile));
+            } catch (IOException e) {
+                AbusedLib.LOGGER.fatal("You done borked something with your config", e);
+            }
+        }
     }
 }
